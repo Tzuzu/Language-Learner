1c665d8a3c4ca55513957438e41929590881de0b
Krystal Richardson <krystal.williams2019@gmail.com>
2023-07-14 00:21:57 -0400

    had to make changes for bugs

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const {User} = require('../../models');
 
-
 router.post('/', async (req, res) => {
     try {
 const newUser = await User.create({
