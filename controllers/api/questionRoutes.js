ee72e1ccaffd2ef2ba2399c67b0ff31880ca9ed8
Krystal Richardson <krystal.williams2019@gmail.com>
2023-07-14 01:21:52 -0400

    edited files

--- controllers/api/questionRoutes.js
+++ controllers/api/questionRoutes.js
@@ -2,35 +2,14 @@ const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const {User, Question} = require('../../models');
 
-router.get('/japanese', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
     const questionData = await Question.findAll();
-    const questions = questionData.map((question) => question.get({ plain: true}));
-    res.render("japanese", { questions });
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).json(error);
-    }
-});
-
-router.get('/french', async (req, res) => {
-    try {
-    const questionData = await Question.findAll();
-    const questions = questionData.map((question) => question.get({ plain: true}));
-    res.render("french", { questions });
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).json(error);
-    }
-});
-
-router.get('/spanish', async (req, res) => {
-    try {
-    const questionData = await Question.findAll();
-    const questions = questionData.map((question) => question.get({ plain: true}));
-    res.render("spanish", { questions });
+    const question = questionData.map((question) => question.get({ plain: true}));
+    res.render("japanese", {
+        question: questionData
+    });
+   res.status()
     }
     catch (error) {
         console.log(error);
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const {User} = require('../../models');
 
+
 router.post('/', async (req, res) => {
     try {
 const newUser = await User.create({
     username: req.body.username,
     email: req.body.email,
     password: req.body.password,
-    languageId: req.body.languageId
 })
 res.status(201).json(newUser);
     } catch (error) {
@@ -47,25 +47,5 @@ router.get('/:userId', async (req, res) => {
         res.status(500).json(error);
     }
 });
-
-// router.put('/userId', async (req, res) => {
-//     try {
-//        const updatedUser = await User.update(req.body, {
-//         where: {
-//             id: req.params.userId
-//         },
-//         individualHooks: true,
-//        }); 
-
-//        if (!updatedUser[0]) return res.status(404).json({message:
-//         'No user found.'})
-
-//        console.log(updatedUser);
-//        res.status(202).json(updatedUser)
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).json(error);
-//     }
-// });
 
 module.exports = router;
