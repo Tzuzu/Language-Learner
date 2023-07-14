d800d98da1f40fa9a4b310e60958565e91acd83c
Tzuzu <natoiodice@gmail.com>
2023-07-13 21:43:49 -0700

    Added seeds

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 //const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
-// const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 // const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -11,26 +10,23 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const hbs = exphbs.create({ helpers }); 
 const hbs = exphbs.create({});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/public', express.static(path.join(__dirname, 'public')));
+const sess = {
+   secret: 'Super secret secret',
+   cookie: {},
+   resave: false,
+   saveUninitialized: true,
+   store: new SequelizeStore({
+     db: sequelize
+   })
+ };
 
-//const sess = {
-   // secret: 'Super secret secret',
-   //cookie: {},
-   // resave: false,
-   // saveUninitialized: true,
-   // store: new SequelizeStore({
-    //  db: sequelize
-   // })
-//  };
-
-//app.use(session(sess));
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
