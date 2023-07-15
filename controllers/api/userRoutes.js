09350e9d2fae27be6f69af6361a2da23f1990a6d
Gcreach <gcreach97@gmail.com>
2023-07-14 20:10:05 -0700

    minor server changes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -48,24 +48,50 @@ router.get('/:userId', async (req, res) => {
     }
 });
 
-// router.put('/userId', async (req, res) => {
-//     try {
-//        const updatedUser = await User.update(req.body, {
-//         where: {
-//             id: req.params.userId
-//         },
-//         individualHooks: true,
-//        }); 
-
-//        if (!updatedUser[0]) return res.status(404).json({message:
-//         'No user found.'})
-
-//        console.log(updatedUser);
-//        res.status(202).json(updatedUser)
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).json(error);
-//     }
-// });
+router.post('/login', async (req, res) => {
+    try {
+      // Find the user who matches the posted e-mail address
+      const userData = await User.findOne({ where: { email: req.body.email } });
+  
+      if (!userData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      // Verify the posted password with the password store in the database
+      const validPassword = await userData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      // Create session variables based on the logged in user
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+        
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
+  
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+  
+  router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+      // Remove the session variables
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const express = require('express');
-//const session = require('express-session');
+const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 
 const sequelize = require('./config/connection');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,19 +16,17 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/public', express.static(path.join(__dirname, 'public')));
+const sess = {
+    secret: 'Super secret secret',
+   cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
 
-//const sess = {
-   // secret: 'Super secret secret',
-   //cookie: {},
-   // resave: false,
-   // saveUninitialized: true,
-   // store: new SequelizeStore({
-    //  db: sequelize
-   // })
-//  };
-
-//app.use(session(sess));
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
