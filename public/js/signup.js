055667e17148e98669d40af56d5ba7b77999ceb9
Krystal Richardson <krystal.williams2019@gmail.com>
2023-07-15 20:51:21 -0400

    trying to fix bugs for login

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,25 +1,23 @@
 const signupFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const name = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('password-signup').value.trim();
+  const email = document.querySelector('#floatingInput').value.trim();
+  const password = document.querySelector('#floatingPassword').value.trim();
+  const confirmPassword = document.querySelector('#floatingPassword2').value.trim();
 
-    if (name && email && password) {
-        const response = await fetch('/api/users', {
-            method: 'POST',
-            body: JSON.stringify({ name, email, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
+  if (email && password && confirmPassword) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
 
-        if (response.ok) {
-          document.location.replace('/');
-        } else {
-          alert(response.statusText);
-        }
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
     }
-}
+  }
+};
 
-document
-    .querySelector('#sign-up')
-    .addEventListener('submit', signupFormHandler);
+document.querySelector('#sign-up').addEventListener('submit', signupFormHandler);
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const express = require('express');
+const fs = require('fs');
 const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
-
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -11,32 +11,43 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const hbs = exphbs.create({ helpers }); 
-const hbs = exphbs.create({});
-
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const sess = {
-   secret: 'Super secret secret',
-   cookie: {},
-   resave: false,
-   saveUninitialized: true,
-   store: new SequelizeStore({
-     db: sequelize
-   })
- };
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
 app.use(session(sess));
 
+const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(routes);
 
-sequelize.sync().then( () => {
-    app.listen(PORT, () => {
-        console.log(`App listening on port ${PORT}`);
-    })
-})
+app.post('/api.users/login', (req, res) => {
+  const { email, password } = req.body;
+  const userData = { email, password };
+  fs.appendFile('userData.json', JSON.stringify(userData), (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Failed to store user data' });
+    } else {
+      res.status(200).json({ message: 'User data stored successfully' });
+    }
+  });
+});
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+  });
+});
