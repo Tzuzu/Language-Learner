5b220cda7ee081f91f7e3c6fab06d8ffb3962f09
Tzuzu <natoiodice@gmail.com>
2023-07-17 21:40:50 -0700

    Added login button

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,13 @@
 var signupBtn = document.getElementById("signup-button")
+var loginBtn = document.getElementById("login-button")
 
 signupBtn.addEventListener("click", signupRedirect);
+loginBtn = document.getElementById("click", loginRedirect);
 
 function signupRedirect() {
     window.location.href = "/signup"
+}
+
+function loginRedirect() {
+    window.location.href = "/login"
 }
